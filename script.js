35493ea5060c0df1608d9c8f3c42c2ca4dee0ae3
Franchuphone <222453306+Franchuphone@users.noreply.github.com>
2026-02-08 14:10:45 +0100

    Fix JS one player mode game Add JS auto relaunch round functionnality Change CSS button behaviour for UX improvement

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function Cell() {
 
 function GameController(
     playerOneName = "Player One",
-    playerTwoName = "Computer"
+    playerTwoName = "Player Two"
 ) {
     const board = Gameboard();
 
@@ -83,9 +83,7 @@ function GameController(
 
         // Check if last play has a winning combination
         // Activate win state on corresponding cells
-
-        const winComb = winConditions( board.getBoard(), activePlayer.token );
-
+        const winComb = winChecker( board.getBoard(), activePlayer.token );
         if ( winComb.getWinState() ) {
             activePlayer.wins += 1;
             let winCells = winComb.getWinCombination();
@@ -112,33 +110,30 @@ function GameController(
 }
 
 function ScreenController() {
-    const game = GameController();
-    const activePlayer = game.getActivePlayer();
+
+    // Initiates game display regarding number of players input
+    let game;
+    const playerChoice = document.querySelector( 'input[name="players"]:checked' );
+    ( playerChoice.value === "1P" ) ? game = GameController( "Player", "Computer" ) : game = GameController();
+
     const playerTurnDiv = document.querySelector( '.player-display' );
     const boardContainer = document.querySelector( ".board-container" );
     const boardDiv = document.querySelector( '.board' );
-    // const btnNewRound = document.createElement( "button" );
-    const scoreBoard = document.querySelector( ".score-board" );
-    // const playerChoiceDiv = document.querySelector( ".player-choice" );
 
     // Global screen changes on game startup
-    // playerChoiceDiv.classList.add( "not-visible" );
-    // btnNewRound.className = "game-ruler";
-    // btnNewRound.textContent = "New round";
     boardContainer.className = "board-container playing-state";
     startGame.getBtnGame().textContent = "Restart Game";
-    startGame.getBtnGame().removeEventListener( "click", ScreenController )
-    startGame.getBtnGame().addEventListener( "click", ScreenController )
-
+    startGame.getBtnGame().removeEventListener( "click", ScreenController );
+    startGame.getBtnGame().addEventListener( "click", ScreenController );
 
     const updateScreen = () => {
         const board = game.getBoard();
         const players = game.getAllPlayers();
 
-        // Update boards on screen update
+        // Update board and score on screen update
         updateScore( players[ 0 ].name, players[ 0 ].wins, players[ 1 ].name, players[ 1 ].wins );
         boardDiv.textContent = "";
-        playerTurnDiv.textContent = `${ activePlayer.name }'s turn`
+        playerTurnDiv.textContent = `${ game.getActivePlayer().name }'s turn`
 
         // Render board squares
         board.forEach( ( cell, index ) => {
@@ -150,73 +145,76 @@ function ScreenController() {
             boardDiv.appendChild( cellButton );
         } );
 
-        // Check if a cell has a winning state and changes its background color
+        // Change cell display if winning conditions met
         let winDisplayState = false;
         game.getBoard().forEach( ( cell, index ) => {
             if ( cell.getCellState() ) {
                 const winCell = document.querySelector( `[data-cell="${ index }"]` )
                 winCell.classList.add( "win-cells" );
                 winDisplayState = true;
-                relaunchGame();
             };
         } );
 
         // End round and launch next
-        // if ( winDisplayState ) {
-        // btnNewRound.addEventListener( "click", () => {
-        //     if ( scoreBoard.lastElementChild.className == "game-ruler" ) scoreBoard.lastElementChild.remove();
-        //     boardDiv.addEventListener( "click", clickHandlerBoard );
-        // } );
-        // scoreBoard.append( btnNewRound );
-        // playerChoiceDiv.classList.remove( "not-visible" );
-        // boardDiv.removeEventListener( "click", clickHandlerBoard );
-        // };
+        if ( winDisplayState ) relaunchGame();
+
+        const getWin = () => winDisplayState;
+
+        return { getWin }
     }
+
+    // Delayed automatic display of new round for UX
+    const delay = ms => new Promise( res => setTimeout( res, ms ) );
 
     async function relaunchGame() {
         boardDiv.removeEventListener( "click", clickHandlerBoard );
-        await delay( 2000 )
+        startGame.getBtnGame().removeEventListener( "click", ScreenController );
+        startGame.getBtnGame().classList.add( "desactivated" );
         const cells = document.querySelectorAll( ".cell" );
+        await delay( 2000 )
         cells.forEach( ( cell ) => {
             cell.textContent = "";
             cell.classList.remove( "win-cells" );
         } );
-        playerTurnDiv.textContent = `${ activePlayer.name }'s turn`;
         game.resetBoard();
-        game.switchPlayerTurn();
+        if ( playerChoice.value === "1P" ) game.switchPlayerTurn();
+        // Automatic computer play if computer starts the round
+        if ( ( game.getActivePlayer().token === "O" ) && ( playerChoice.value === "1P" ) ) handleComputerRound( game.getBoard(), game, playerChoice );
+        updateScreen();
         boardDiv.addEventListener( "click", clickHandlerBoard );
+        startGame.getBtnGame().addEventListener( "click", ScreenController );
+        startGame.getBtnGame().classList.remove( "desactivated" );
     }
 
-    async function handleRelaunch() {
+    // Check for a draw
+    function handleRelaunch() {
         let count = 0;
         game.getBoard().forEach( ( cell ) => {
-            // console.log( cell.getValue() )
             if ( cell.getValue() === "" ) count += 1
         } );
-        console.log( count )
-        if ( count === 0 ) relaunchGame()
+        if ( count === 0 ) relaunchGame();
     }
 
     // Add event listener for the board
-    function clickHandlerBoard( e ) {
-        if ( scoreBoard.lastElementChild.className == "game-ruler" ) scoreBoard.lastElementChild.remove()
+    async function clickHandlerBoard( e ) {
         const selectedCell = e.target.dataset.cell;
         if ( !selectedCell ) return;
-        game.playRound( selectedCell );
+        // Prevent malfunctions by clicking on an occupied cell
+        if ( game.getBoard()[ selectedCell ].getValue() === ( "O" || "X" ) ) return
         handleRelaunch();
-        handleComputerRound( game.getBoard(), game );
-        updateScreen();
+        game.playRound( selectedCell );
+        if ( playerChoice.value === "1P" ) handleComputerRound( game.getBoard(), game, playerChoice );
+        if ( !updateScreen().getWin() ) handleRelaunch();
     };
 
     boardDiv.addEventListener( "click", clickHandlerBoard )
-    // ( game.getActivePlayer().token === "X" ) ? boardDiv.addEventListener( "click", clickHandlerBoard ) : handleComputerRound( game.getBoard(), game );
 
     updateScreen();
-
 }
 
-function winConditions( board, token ) {
+function winChecker( board, token ) {
 
+    // List all possible winning combination positions
     let possibleCombinations = [
         [ 0, 1, 2 ],
         [ 0, 4, 8 ],
@@ -277,46 +275,36 @@ function updateScore( user1, text1, user2, text2 ) {
     }
 };
 
-function sleep( ms ) {
-    let timeStart = new Date().getTime();
-    while ( true ) {
-        let elapsedTime = new Date().getTime() - timeStart;
-        if ( elapsedTime > ms ) {
-            break;
-        }
-    }
-}
-
-function wait( ms ) {
-    return new Promise( resolve => setTimeout( resolve, ms ) )
-}
-
-function handleComputerRound( board, game ) {
-    const playerChoice = document.querySelector( 'input[name="players"]:checked' );
+function handleComputerRound( board, game, choice ) {
     let emptyCells = [];
     let stop;
     board.forEach( ( cell ) => {
         if ( cell.getCellState() ) return stop = true;
     } );
     if ( stop ) {
-        console.log( "acrivates" )
         game.switchPlayerTurn();
         return;
     }
-    if ( playerChoice.value === "2P" ) {
+    if ( choice.value === "1P" ) {
         board.forEach( ( cell, index ) => {
-            console.log( cell.getValue() )
             if ( cell.getValue() === "" ) emptyCells.push( index );
         } );
         let rand = Math.floor( Math.random() * emptyCells.length );
         if ( emptyCells[ rand ] !== undefined ) game.playRound( emptyCells[ rand ] );
+    }
+    board.forEach( ( cell ) => {
+        if ( cell.getCellState() ) return stop = true;
+    } );
+    if ( stop ) {
+        game.switchPlayerTurn();
+        return;
     }
 }
 
 const startGame = ( function () {
     const btnGame = document.querySelector( ".game-ruler" );
 
-    btnGame.addEventListener( "click", ScreenController )
+    btnGame.addEventListener( "click", ScreenController );
 
     const getBtnGame = () => btnGame;
 
@@ -324,5 +312,3 @@ const startGame = ( function () {
         getBtnGame,
     }
 } )();
-
-const delay = ms => new Promise( res => setTimeout( res, ms ) );
